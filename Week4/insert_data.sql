c2b2b149895292daf531482def38bc8ab99db376
KelvinMein <meinkelvinto@gmail.com>
2026-06-04 22:27:58 +0300

    Add initial data inserts for admin, users, and products

---
+++ Week4/insert_data.sql
@@ -0,0 +1,16 @@
+USE ecommerce_system;
+
+INSERT INTO admin (username, password)
+VALUES ('admin', '1234');
+
+INSERT INTO users (username, password)
+VALUES 
+('john', '1234'),
+('mary', '1111');
+
+INSERT INTO products (name, price)
+VALUES 
+('Laptop', 60000),
+('Phone', 25000),
+('Headphones', 3500),
+('Woofer', 12000);
